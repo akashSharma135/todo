532126da5298f8d4092ab6b93571384c26832499
akashsharma135 <akashsharma1352000@gmail.com>
2021-09-13 16:45:21 +0530

    decorator created and used serializers

--- users/utils.py
+++ users/utils.py
@@ -5,9 +5,3 @@ class admin_required(BasePermission):
         if request.user.role == 'admin':
             return True
         return False
-
-class manager_required(BasePermission):
-    def has_permission(self, request, view):
-        if request.user.role == 'manager':
-            return True
-        return False
